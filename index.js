7822186789b10f0b3b4bde5a04a915988612d80d
KamilSwierdzewski <151411926+KamilSwierdzewski@users.noreply.github.com>
2023-12-03 21:20:24 +0000

    scrimba advent challenge
    simple xmas countdown timer

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const countdownDisplay = document.getElementById("countdown-display");
-const christmas = new Date("DEC 25, 2023, 00:00:00");
+const countdownText = document.getElementById("countdown-text");
+const christmas = new Date("Dec 25, 2023, 00:00:00").getTime();
+let year = new Date().getFullYear();
 
-var x = setInterval(function timer() {
-  var now = new Date();
-  var timeLeft = christmas - now;
-  var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+let x = setInterval(function timer() {
+  let now = new Date();
+  let timeLeft = christmas - now;
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
   countdownDisplay.innerHTML =
     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  /*if (days < -2){
+        christmas = new Date("DEC 25, " + (year + 1) + "00:00:00").getTime();
+    }*/
+  if (timeLeft < 0) {
+    clearInterval(x);
+    countdownDisplay.innerHTML = "Ho-Ho-Ho";
+    countdownText.innerHTML = "It's Christmas";
+  }
 });
